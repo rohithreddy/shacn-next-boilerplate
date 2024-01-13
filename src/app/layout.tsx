d0fd02548400481cb80516702695fca7b0c2a0d9
crash <chaoscrashx@gmail.com>
2024-01-13 21:15:33 +0530

    orange and black newyork theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.className)}>{children}</body>
+      <body className={cn("dark min-h-screen bg-background font-sans antialiased", inter.className)}>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import { Button } from "@/components/ui/button"
+import { cn } from '@/lib/utils'
+
+
 
 export default function Home() {
   return (
     <div>
+      <p className={cn("text-foreground")}>
+      This is a boiler plate code for the dark themed orange
+      </p>
+      
       <Button>Click me</Button>
     </div>
   )
